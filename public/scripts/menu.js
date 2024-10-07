ee38e4b0fb7494a2685a0abdbabe10c6c7c20ba0
bobkae <denisdipp3@gmail.com>
2024-10-07 18:26:45 +0200

    EmtyChangesYaRotEbal

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
 
-    const btnBurgerMenu = document.getElementById('btnBurgerMenu');
-    const crossBurgerMenu = document.getElementById('crossBurgerMenu');
-    const menu = document.querySelector('.menu');
+//     const btnBurgerMenu = document.getElementById('btnBurgerMenu');
+//     const crossBurgerMenu = document.getElementById('crossBurgerMenu');
+//     const menu = document.querySelector('.menu');
 
-    btnBurgerMenu.addEventListener('click', function () {
-        menu.classList.toggle('d-none');
-        menu.classList.toggle('d-active');
-        btnBurgerMenu.classList.toggle('d-none');
-        crossBurgerMenu.classList.toggle('d-none');
-    });
+//     btnBurgerMenu.addEventListener('click', function () {
+//         menu.classList.toggle('d-none');
+//         menu.classList.toggle('d-active');
+//         btnBurgerMenu.classList.toggle('d-none');
+//         crossBurgerMenu.classList.toggle('d-none');
+//     });
 
-    crossBurgerMenu.addEventListener('click', function () {
-        menu.classList.toggle('d-none');
-        menu.classList.toggle('d-active');
-        btnBurgerMenu.classList.toggle('d-none');
-        crossBurgerMenu.classList.toggle('d-none');
-    });
-});
+//     crossBurgerMenu.addEventListener('click', function () {
+//         menu.classList.toggle('d-none');
+//         menu.classList.toggle('d-active');
+//         btnBurgerMenu.classList.toggle('d-none');
+//         crossBurgerMenu.classList.toggle('d-none');
+//     });
+// });
 
