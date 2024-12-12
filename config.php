f691b1e6be75c43d51c104572b6922b70070af97
Imrazina <annasabossova@gmail.com>
2024-12-12 21:24:20 +0100

    chatic

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+// Настройки подключения к базе данных
+$host = 's554ongw9quh1xjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$dbname = 'hoc3ablulex394pb';
+$username = 'emk2ggh76qbpq4ml';
+$password = 'lf9c0g2qky76la6x';
+
+// Создаем подключение через MySQLi
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Проверяем подключение
+if ($conn->connect_error) {
+    die("Ошибка подключения: " . $conn->connect_error);
+}
+?>
